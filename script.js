e52c9b6a5de87830706766b9997249d71d332159
zephkelly <masterevank@gmail.com>
2022-12-22 15:25:30 +1100

    Finish gameplay logic and win states

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let currentPlayer
+let gameOver = false
+
+const gameStatus = document.getElementById("game-status")
 
 //Player factory function
 const playerFactory = (name, symbol) => {
@@ -16,12 +19,12 @@ const gameboard = (() => {
     return gameboard
   }
 
-  const setGameboard = (x, y, symbol) => {
+  const makePlay = (x, y, symbol) => {
     gameboard[x][y] = symbol
     console.log(gameboard)
   }
 
-  return { getGameboard, setGameboard }
+  return { getGameboard, makePlay }
 })()
 
 function buildGameboard() {
@@ -39,19 +42,92 @@ function buildGameboard() {
 //Game module
 const gameManager = (() => {
   currentPlayer = player1
+  gameStatus.textContent = `${currentPlayer.symbol}'s turn`
 
-  const play = (x, y) => {
-    gameboard.setGameboard(x, y, currentPlayer.symbol)
+  const play = (x, y, targetDiv) => {
+    if (canMakePlay(x, y) == false) return
+
+    gameboard.makePlay(x, y, currentPlayer.symbol)
+    targetDiv.target.innerHTML = currentPlayer.symbol
+
     currentPlayer = currentPlayer === player1 ? player2 : player1
-
+    gameStatus.textContent = `${currentPlayer.symbol}'s turn`
     checkWinner()
+  }
+
+  function canMakePlay(x, y) {
+    if (gameboard.getGameboard()[x][y] == "") return true
+    else return false
   }
 
   const checkWinner = () => {
     //TODO
+
+    //Check rows
+    for (let i = 0; i < 3; i++) {
+      if (
+        gameboard.getGameboard()[i][0] == gameboard.getGameboard()[i][1] &&
+        gameboard.getGameboard()[i][1] == gameboard.getGameboard()[i][2] &&
+        gameboard.getGameboard()[i][0] != ""
+      ) {
+        endGame()
+      }
+    }
+
+    //Check columns
+    for (let i = 0; i < 3; i++) {
+      if (
+        gameboard.getGameboard()[0][i] == gameboard.getGameboard()[1][i] &&
+        gameboard.getGameboard()[1][i] == gameboard.getGameboard()[2][i] &&
+        gameboard.getGameboard()[0][i] != ""
+      ) {
+        endGame()
+      }
+    }
+
+    //Check diagonals
+    if (
+      gameboard.getGameboard()[0][0] == gameboard.getGameboard()[1][1] &&
+      gameboard.getGameboard()[1][1] == gameboard.getGameboard()[2][2] &&
+      gameboard.getGameboard()[0][0] != ""
+    ) {
+      endGame()
+    }
+
+    if (
+      gameboard.getGameboard()[0][2] == gameboard.getGameboard()[1][1] &&
+      gameboard.getGameboard()[1][1] == gameboard.getGameboard()[2][0] &&
+      gameboard.getGameboard()[0][2] != ""
+    ) {
+      endGame()
+    }
+
+    //Check for draw
+    let draw = true
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (gameboard.getGameboard()[i][j] == "") draw = false
+      }
+    }
+
+    if (draw) endGame(true)
+
+    return
   }
 
-  const endGame = () => {}
+  const endGame = (draw = false) => {
+    gameOver = true
+    document.getElementById("reset").textContent = "Play again"
+
+    if (draw) {
+      gameStatus.textContent = `It's a draw!`
+      return
+    } else {
+      //Change the current player to be the winner
+      currentPlayer = currentPlayer === player1 ? player2 : player1
+      gameStatus.textContent = `${currentPlayer.symbol} is the winner!`
+    }
+  }
 
   return { play, checkWinner, endGame }
 })()
@@ -60,5 +136,13 @@ document.getElementById("gameboard").addEventListener("click", function (e) {
   let x = e.target.getAttribute("data-x")
   let y = e.target.getAttribute("data-y")
 
-  gameManager.play(x, y)
+  //x and y are reversed due to gameboard storage structure
+  if (gameOver) return
+  gameManager.play(y, x, e)
 })
+
+const resetButton = document
+  .getElementById("reset")
+  .addEventListener("click", function (e) {
+    location.reload()
+  })
